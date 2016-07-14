011d4c03f4839a30f9414652dfd7a9fbd2ccadeb
Alain Chautard <achautard@gmail.com>
2016-07-14 14:04:13 -0700

    no message

--- 5-class.ts
+++ 5-class.ts
@@ -22,14 +22,3 @@ function sayHello(person: Person) : string {
 let user = new Employee("Alain", "R", "Chautard");
 
 document.body.innerHTML = sayHello(user);
-
-
-class Grid {
-    static origin = {x: 0, y: 0};
-    calculateDistanceFromOrigin(point: {x: number; y: number;}) {
-        let xDist = (point.x - Grid.origin.x);
-        let yDist = (point.y - Grid.origin.y);
-        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
-    }
-    constructor (public scale: number) { }
-}

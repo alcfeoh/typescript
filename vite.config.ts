dccc2f827426c8120e5c10943a2bcbe942f51f96
alcfeoh <alain@interstate21.com>
2026-09-01 10:49:30 +0200

    TS build feedback added

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
+  plugins: [
+    checker({
+      typescript: true,
+    }),
+  ],
   server: {
     port: 3000
   },
